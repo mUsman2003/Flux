d1139246e94869052ef8f97b8510a04563773495
mUsman2003 <musman2003.fb@gmail.com>
2025-03-21 15:58:49 +0500

    db-conection-fixed

--- backend/db/db.js
+++ backend/db/db.js
@@ -5,12 +5,12 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE, // Matches `.env`
   port: process.env.DB_PORT,
 });
 
 pool.connect()
-  .then(() => console.log('Connected to PostgreSQL'))
+  .then(() => console.log('Connected to PostgreSQL 17'))
   .catch(err => console.error('Connection error:', err));
 
 module.exports = pool;
