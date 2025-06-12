038caaf0a061e56218dfd7e7c01ac3c547d95951
mUsman2003 <usman2003.fb@gmail.com>
2025-06-12 22:47:57 +0500

    removed argodownload

--- actions-runner/_work/_update.cmd
+++ actions-runner/_work/_update.cmd
@@ -11,12 +11,12 @@ rem  UPDATELOG = _diag/SelfUpdate-UTC.log
 rem  RESTARTINTERACTIVERUNNER = 0/1
 
 setlocal
-set runnerpid=23652
+set runnerpid=14816
 set runnerprocessname=Runner.Listener.exe
 set rootfolder=D:\BS(CS)\Flux\actions-runner
 set existrunnerversion=2.317.0
-set downloadrunnerversion=2.324.0
-set logfile=D:\BS(CS)\Flux\actions-runner\_diag\SelfUpdate-20250528-155123.log
+set downloadrunnerversion=2.325.0
+set logfile=D:\BS(CS)\Flux\actions-runner\_diag\SelfUpdate-20250612-173200.log
 set restartinteractiverunner=0
 
 rem log user who run the script
